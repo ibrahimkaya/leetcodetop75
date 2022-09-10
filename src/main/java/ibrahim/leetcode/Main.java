ca30c57d8fc010970fb9cb38ecde2b5455a4d916
ibrahimkaya <kayaibrahim0@yandex.com>
2022-09-10 11:27:34 +0300

    initial repository

---
+++ src/main/java/ibrahim/leetcode/Main.java
@@ -0,0 +1,14 @@
+package ibrahim.leetcode;
+
+/**
+ * @Author ibrahim
+ * @create 9.09.2022 21:11
+ * <p>
+ * <a href="https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU">...</a>
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
